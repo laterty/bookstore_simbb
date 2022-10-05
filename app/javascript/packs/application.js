a7b1de0c39c735b6cfc3ffaef7f6243fa024db24
max <laterty@gmail.com>
2022-10-05 19:15:24 +0300

    fix rubocop issue and remove unnecessary js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require jquery
-require bootstrap-sprockets
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
