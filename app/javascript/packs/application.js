8316b7033e32b256a0873f7c561367dd6ba93790
max <laterty@gmail.com>
2022-10-09 19:05:02 +0300

    fix js import for heroku deploy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
-import './src/application.scss'
+// import './src/application.scss'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,3 +124,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+
+
