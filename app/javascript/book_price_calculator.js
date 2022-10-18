eae42154e4d8db7d8946241554d6f861240381c6
max <laterty@gmail.com>
2022-10-18 20:07:08 +0300

    remove console log

--- app/javascript/book_price_calculator.js
+++ app/javascript/book_price_calculator.js
@@ -1,10 +1,11 @@
 document.addEventListener("turbolinks:load", function() {
   $('#plus_books_price').click(function (e) {
     this.parentNode.querySelector('#books_price').stepUp()
+    e.preventDefault();
   });
 
   $('#minus_books_price').click(function (e) {
-    console.log('hey');
     this.parentNode.querySelector('#books_price').stepDown()
+    e.preventDefault();
   });
 });
