457b9f3bbd2343dc57ec82810664fe300ad33e76
max <laterty@gmail.com>
2022-10-19 18:31:52 +0300

    fix feature tests

--- spec/features/show_book_spec.rb
+++ spec/features/show_book_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Book page', type: :feature do
   let(:book) { create(:book) }
 
-  it 'truncate books description' do
+  it 'truncate books description', js: true do
     visit book_path(book)
 
     expect(page).to have_css('#truncate_description')
+    expect(page).to have_no_css('#full_description')
   end
 
-  it 'show full description after click link' do
+  it 'show full description after click link', js: true do
     visit book_path(book)
 
     find_by_id('book_read_more').click
-
     expect(page).to have_no_css('#truncate_description')
   end
 end
