7a647edfd6e137dd80b4f81350db318a52b6b276
max <laterty@gmail.com>
2022-10-26 21:16:26 +0300

    change email_regex validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   devise :omniauthable, omniauth_providers: %i[facebook]
 
+  validates :email, :password, presence: true
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.email = auth.info.email
