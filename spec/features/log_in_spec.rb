88bafde26d65e29cc44069fe1cea08d158dd8575
max <laterty@gmail.com>
2022-10-27 23:44:25 +0300

    add feature test for forgot_password case

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -15,4 +15,19 @@ feature 'log in' do
     expect(page).to have_content 'Log out'
     expect(page).to have_content 'Signed in successfully.'
   end
+
+  scenario 'Visitor forgots password' do
+    visit new_user_session_path
+    click_link('Forgot password?')
+    expect(page.current_path).to eq new_user_password_path
+
+    within '#new_user' do
+      fill_in 'user_email', with: @user.email
+      click_button('Email Instructions')
+    end
+    expect(page.current_path).to eq new_user_session_path
+    expect(page).to have_content 'You will receive an email with instructions on how to reset your password in a few minutes.'
+  end
 end
+
+
