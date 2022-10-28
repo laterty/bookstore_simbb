dfe58415eea946bfbafa5b71845e014c62cd18d5
max <laterty@gmail.com>
2022-10-28 15:29:48 +0300

    add routes to settings and small refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   get 'addresses/index'
+  root 'home#index'
+  get 'settings/addresses', to: 'addresses#index'
+  get 'settings/privacy', to: 'users#edit'
+  resources :books, only: %i(index show update)
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions'
   }
 
-  resources :books
-  root 'home#index'
-
   devise_scope :user do
     get '/users', to: 'devise/registrations#new'
   end
+
 end
