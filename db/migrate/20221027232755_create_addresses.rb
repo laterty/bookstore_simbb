397b2352a37f07832a7ae5c7ec1fae08f6f834b4
max <laterty@gmail.com>
2022-10-28 15:54:33 +0300

    add migration for addresses

--- db/migrate/20221027232755_create_addresses.rb
+++ db/migrate/20221027232755_create_addresses.rb
@@ -1,8 +1,16 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :city
+      t.string :country
+      t.string :zip
+      t.string :phone
+      t.string :type
       t.timestamps
+      t.references :user, foreign_key: true
     end
   end
 end
