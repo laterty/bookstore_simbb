f51f484fc7a5a4eeffdcbad5a812221069436332
max <laterty@gmail.com>
2022-10-31 11:12:38 +0200

    add simple_form for addresses

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'settings/addresses', to: 'addresses#index'
   get 'settings/privacy', to: 'users#edit'
+  post 'settings/addresses', to: 'addresses#create'
   resources :books, only: %i(index show update)
 
   devise_for :users, controllers: {
