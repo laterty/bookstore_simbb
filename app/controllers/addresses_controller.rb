4fde9007312affee6a16e380d3c8074963645a83
max <laterty@gmail.com>
2022-11-07 01:38:43 +0200

    fix update address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class AddressesController < ApplicationController
-  def edit
-  end
+  def edit; end
 
   def create
     if typed_form.save
       redirect_to edit_settings_address_path
-    else 
+    else
       flash[:alert] = 'IT WORKS DAMN'
       render :edit
     end
@@ -16,32 +15,31 @@ class AddressesController < ApplicationController
   def update
     if typed_form.save
       redirect_to edit_settings_address_path
-    else 
+    else
       flash[:alert] = 'IT WORKS DAMN'
       render :edit
     end
   end
-=begin
-  def shipping_address_locals
-    if current_user.shipping_address
-      return { address: @shipping_address_form.nil? ? current_user.shipping_address : @shipping_address_form , type: 'ShippingAddress' }
-    else
-      return { address: @shipping_address_form.nil? ? AddressForm.new(Address.new(type: 'ShippingAddress')) : @shipping_address_form , type: 'ShippingAddress' }
-    end
-  end
+  #   def shipping_address_locals
+  #     if current_user.shipping_address
+  #       return { address: @shipping_address_form.nil? ? current_user.shipping_address : @shipping_address_form , type: 'ShippingAddress' }
+  #     else
+  #       return { address: @shipping_address_form.nil? ? AddressForm.new(Address.new(type: 'ShippingAddress')) : @shipping_address_form , type: 'ShippingAddress' }
+  #     end
+  #   end
+  #
+  #   def billing_address_locals
+  #     if current_user.billing_address
+  #       return { address: @billing_address_form.nil? ? current_user.billing_address : @billing_address_form , type: 'BillingAddress' }
+  #     else
+  #       return { address: @billing_address_form.nil? ? AddressForm.new(Address.new(type: 'BillingAddress')) : @billing_address_form , type: 'BillingAddress' }
+  #     end
+  #   end
 
-  def billing_address_locals
-    if current_user.billing_address
-      return { address: @billing_address_form.nil? ? current_user.billing_address : @billing_address_form , type: 'BillingAddress' }
-    else
-      return { address: @billing_address_form.nil? ? AddressForm.new(Address.new(type: 'BillingAddress')) : @billing_address_form , type: 'BillingAddress' }
-    end
-  end
-=end
   private
 
   def typed_form
-    if has_billing_type?
+    if billing_type?
       @billing_address_form = AddressForm.new(address, address_params)
     else
       @shipping_address_form = AddressForm.new(address, address_params)
@@ -49,10 +47,14 @@ class AddressesController < ApplicationController
   end
 
   def address
-    @address ||= Address.new(user: current_user, type: address_params[:type])
+    @address ||= if Address.where(user: current_user, type: address_params[:type]).any?
+                   Address.find_by(user: current_user, type: address_params[:type])
+                 else
+                   Address.new(user: current_user, type: address_params[:type])
+                 end
   end
 
-  def has_billing_type?
+  def billing_type?
     address_params[:type] == 'BillingAddress'
   end
 
