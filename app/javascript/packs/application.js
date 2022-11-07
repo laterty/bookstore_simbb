c1ba03ebcb9db6127aa297c7f5b950aa372ca47c
max <laterty@gmail.com>
2022-11-07 18:06:54 +0200

    move styles to js folder (by webpacker)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
-// import './src/application.scss'
+
+import 'jquery'
+import 'popper.js'
+import 'bootstrap-sass'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import '../styles/application'
