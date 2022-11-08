0b3926708a93f3eb4a620c1d17e0d47302a9b70a
max <laterty@gmail.com>
2022-11-08 23:01:55 +0200

    form for updating password works now

--- app/controllers/update_user_passwords_controller.rb
+++ app/controllers/update_user_passwords_controller.rb
@@ -1,10 +1,10 @@
-class UpdateUserPasswordController < ApplicationController
+class UpdateUserPasswordsController < ApplicationController
 
   def update
     @password_form = UpdateUserPasswordForm.new(current_user, permitted_params)
     if @password_form.save
       flash[:notice] = 'Password was successfully changed!'
-      redirect_to user_path
+      redirect_to new_user_session_path
     else
       render :edit
     end
--- app/forms/update_user_password_form.rb
+++ app/forms/update_user_password_form.rb
@@ -1,4 +1,27 @@
 class UpdateUserPasswordForm < ApplicationForm
     attr_accessor :current_password, :password, :password_confirmation
     
+    validate :password_validation
+
+    def save
+      return unless valid?
+
+      @model.update(password: @params[:password])
+      @model
+    end
+
+    private
+    
+    def password_validation
+      new_password_confirmed? && old_password_valid?
+    end
+
+    def new_password_confirmed?
+      password == password_confirmation
+    end
+
+    def old_password_valid?
+      @model.valid_password?(current_password)
+    end
+
   end
