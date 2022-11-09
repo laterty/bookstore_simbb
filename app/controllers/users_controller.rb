674658e62cddf9f08abcad4b5d27a51ed8fe828c
max <laterty@gmail.com>
2022-11-09 17:29:55 +0200

    delete user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
   def edit; end
+
+  def destroy
+    current_user.destroy if permmit_params[:check_delete_user]
+    redirect_to root_path, notice: 'User was successfully deleted'
+  end
+
+  private
+
+  def permmit_params
+    params.require(:user).permit(:check_delete_user)
+  end
 end
