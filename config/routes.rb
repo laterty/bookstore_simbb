9fd0e790e4209e20582a0e2fe638f02cea17e2a6
max <laterty@gmail.com>
2022-11-09 20:28:02 +0200

    add fix for url after failing settings forms validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   resource :settings, only: [] do
-    resource :address, only: %w[update create edit]
+    resource :address, only: %w[create edit]
     get 'privacy', to: 'users#edit'
+    get 'address', to: redirect('settings/address/edit')
   end
 
   resources :books, only: %i[index show update]
@@ -18,4 +19,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users', to: 'devise/registrations#new'
   end
+
+  # CHECK: to avoid app error when refresh forms page after failing validation:
+  # alternative way: https://fullstackheroes.com/tutorials/rails/wrong-url-validation-error/
+  get 'update_user_email', to: redirect('/user/edit')
+  get 'update_user_password', to: redirect('/user/edit')
 end
