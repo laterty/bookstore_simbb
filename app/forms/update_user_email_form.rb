15f253012753d67eb44ef643411f02c6b938b50d
max <laterty@gmail.com>
2022-11-09 20:28:44 +0200

    add validation for email form

--- app/forms/update_user_email_form.rb
+++ app/forms/update_user_email_form.rb
@@ -1,5 +1,10 @@
 class UpdateUserEmailForm < ApplicationForm
   attr_accessor :email
 
-  validates :email, presence: true, length: { minimum: 3, maximum: 63 }
+  EMAIL_REGEXP = URI::MailTo::EMAIL_REGEXP
+
+  validates :email, 
+    presence: true, 
+    length: { minimum: 3, maximum: 63 }, 
+    format: { with: EMAIL_REGEXP, message: 'Invalid email'}
 end
