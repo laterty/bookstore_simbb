c52f8fd1d650f038b0689644ad40f7c1e87f0bac
max <laterty@gmail.com>
2022-11-09 23:02:45 +0200

    fix password validation

--- app/forms/update_user_password_form.rb
+++ app/forms/update_user_password_form.rb
@@ -1,8 +1,19 @@
 class UpdateUserPasswordForm < ApplicationForm
   attr_accessor :current_password, :password, :password_confirmation
 
-  validate :password_validation
+  validates :password, :password_confirmation, :current_password, presence: true
+  validate :old_password_validation
+  validate :new_password_confirmation
   validate :password_complexity
+
+  def save
+    return unless valid?
+ 
+    @model.update(password: @params[:password])
+    @model
+  end
+
+  private
 
   def password_complexity
     return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
@@ -11,31 +22,11 @@ class UpdateUserPasswordForm < ApplicationForm
                'Complexity requirement not met. Please use: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
   end
 
-  def password_validation
-    password_attributes_not_blank && new_password_confirmed && old_password_valid
+  def new_password_confirmation
+    errors.add :password_confirmation, 'Password confirmation should be the same with new password' unless password == password_confirmation
   end
 
-  def save
-    return unless valid?
-
-    @model.update(password: @params[:password])
-    @model
-  end
-
-  private
-
-  def password_attributes_not_blank
-    return if current_password.present? && password.present? && password_confirmation.present?
-
-    errors.add :password, 'Cant be blank'
-  end
-
-  def new_password_confirmed
-    errors.add :password, 'Password confirmation should be the same with new password' unless password == password_confirmation
-  end
-
-  def old_password_valid
+  def old_password_validation
     errors.add :current_password, 'Wrong current password' unless @model.valid_password?(current_password)
   end
-
 end
