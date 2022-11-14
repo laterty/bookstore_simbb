1f0ea518ecf704e5bb5f08a32f93d116d22aa869
max <laterty@gmail.com>
2022-11-15 01:14:36 +0200

    factories for author, book and authors_book

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -2,5 +2,6 @@
 
 FactoryBot.define do
   factory :author do
+    name { Faker::Book.unique.author }
   end
 end
--- spec/factories/authors_books.rb
+++ spec/factories/authors_books.rb
@@ -2,5 +2,7 @@
 
 FactoryBot.define do
   factory :authors_book do
+    book { Book.all.sample }
+    author { Author.all.sample }
   end
 end
