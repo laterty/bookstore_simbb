6fbabbce21b6693eb0b6c3be48333e082c42a54b
max <laterty@gmail.com>
2022-11-15 01:25:58 +0200

    add null:false to migration; seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,14 @@
 require 'factory_bot_rails'
 
 Book.destroy_all
+Category.destroy_all
+Author.destroy_all
+AuthorsBook.destroy_all
+
+['Photo', 'Web-development', 'Mobile-design'].each { Category.create(name: _1) }
+
+FactoryBot.create_list(:author, 30)
 
 FactoryBot.create_list(:book, 40)
+
+FactoryBot.create_list(:authors_book, 40)
