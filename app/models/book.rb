afffb30ff6f39fb5df1f823e7b7226daaa9b0cec
max <laterty@gmail.com>
2022-11-15 01:24:02 +0200

    fix associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,13 +3,12 @@
 class Book < ApplicationRecord
 
   belongs_to :category
-  has_many :author_books, dependent: :destroy
-  has_many :authors, through: :author_books
+  has_many :authors_books, dependent: :destroy
+  has_many :authors, through: :authors_books
 
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: true
-  validates :author, presence: true
 
   paginates_per 12
 
