628ef9650ece83aaeeb076720e37abe88a6e0865
max <laterty@gmail.com>
2022-11-14 21:16:50 +0200

    add association for Category and Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
+
+  belongs_to :category
   has_many :author_books, dependent: :destroy
   has_many :authors, through: :author_books
 
