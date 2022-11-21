85f4c0036f4d106ace0e9e8d230f01776b356724
max <laterty@gmail.com>
2022-11-21 18:35:42 +0200

    fix books migration and related things

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,4 @@
 
 class Book < ApplicationRecord
   validates :title, presence: true
-  validates :description, presence: true
-  validates :price, presence: true, numericality: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,7 +5,5 @@ RSpec.describe Book, type: :model do
     let(:book) { create(:book) }
 
     it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to validate_presence_of(:description) }
-    it { is_expected.to validate_presence_of(:price) }
   end
 end
