880dd570b044c705bb389039f5f5914733028ac0
max <laterty@gmail.com>
2022-11-23 23:10:33 +0200

    improve routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :books
+
+  resources :books, only: %I[index show]
+  resources :categories, only: %I[show] do
+    resources :books, only: %I[index]
+  end
+  
   root 'home#index'
 end
