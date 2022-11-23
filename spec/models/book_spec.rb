6083ac4540fb7a96812b4ec2aa4fefe73275efa5
max <laterty@gmail.com>
2022-11-23 13:55:42 +0200

    fixes

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Book, type: :model do
   describe 'table' do
-    it { is_expected.to have_db_column(:title).of_type(:string) }
+    it { is_expected.to have_db_column(:title).of_type(:string).with_options(null: false) }
   end
 
   describe 'validations' do
