58e965dc477b174eed319e058db51db92b896c01
max <laterty@gmail.com>
2022-11-25 16:32:10 +0200

    add models specs

--- spec/models/author_book_spec.rb
+++ spec/models/author_book_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe AuthorBook, type: :model do
     it { is_expected.to belong_to(:author) }
     it { is_expected.to belong_to(:book) }
   end
+
+  describe 'columns' do
+    it { is_expected.to have_db_column(:book_id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:author_id).of_type(:integer).with_options(null: false) }
+  end
+
+  describe 'table index' do
+    it { is_expected.to have_db_index(%i[author_id book_id]) }
+  end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Author, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
   end
+
+  describe 'columns' do
+    it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:books).through(:author_books) }
+    it { is_expected.to have_many(:author_books).dependent(:destroy) }
+  end
 end
