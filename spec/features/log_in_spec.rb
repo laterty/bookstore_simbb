554a349aa276970e52f04774ad32516ef9a466fd
max <laterty@gmail.com>
2022-11-26 16:43:05 +0200

    add locales

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -1,33 +1,39 @@
-feature 'log in' do
-  background do
-    @user = FactoryBot.create(:user)
+# frozen_string_literal: true
+
+RSpec.describe 'Log in', type: :feature do
+  let(:user) { create(:user) }
+
+  describe 'Visitor logins successfully via log in form' do
+    before do
+      visit new_user_session_path
+      within '#new_user' do
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: user.password
+        click_button('Log in')
+      end
+    end
+
+    it do
+      expect(page).to have_current_path root_path, ignore_query: true
+      expect(page).not_to have_content 'Log in'
+      expect(page).to have_content 'Log out'
+      expect(page).to have_content 'Signed in successfully.'
+    end
   end
 
-  scenario 'Visitor logins successfully via log in form' do
-    visit new_user_session_path
-    within '#new_user' do
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-      click_button('Log in')
+  describe 'Visitor forgots password' do
+    before do
+      visit new_user_session_path
+      click_link('Forgot password?')
+      within '#new_user' do
+        fill_in 'user_email', with: user.email
+        click_button('Email Instructions')
+      end
     end
-    expect(page.current_path).to eq root_path
-    expect(page).not_to have_content 'Log in'
-    expect(page).to have_content 'Log out'
-    expect(page).to have_content 'Signed in successfully.'
-  end
 
-  scenario 'Visitor forgots password' do
-    visit new_user_session_path
-    click_link('Forgot password?')
-    expect(page.current_path).to eq new_user_password_path
-
-    within '#new_user' do
-      fill_in 'user_email', with: @user.email
-      click_button('Email Instructions')
+    it do
+      expect(page).to have_current_path new_user_session_path, ignore_query: true
+      expect(page).to have_content 'You will receive an email with instructions on how to reset your password in a few minutes.'
     end
-    expect(page.current_path).to eq new_user_session_path
-    expect(page).to have_content 'You will receive an email with instructions on how to reset your password in a few minutes.'
   end
 end
-
-
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,5 +1,7 @@
-feature 'Registration' do
-  scenario 'Visitor registers successfully via register form' do
+# frozen_string_literal: true
+
+describe 'Registration' do
+  it 'Visitor registers successfully via register form' do
     visit new_user_registration_path
     within '#new_user' do
       fill_in 'Email', with: Faker::Internet.safe_email
@@ -7,7 +9,7 @@ feature 'Registration' do
       fill_in 'Confirm password', with: 'Yo12*lo34'
       click_button('Sign up')
     end
-    expect(page.current_path).to eq root_path
+    expect(page).to have_current_path root_path, ignore_query: true
     expect(page).not_to have_content 'Sign up'
     expect(page).to have_content 'Log out'
     expect(page).to have_content 'Welcome! You have signed up successfully.'
