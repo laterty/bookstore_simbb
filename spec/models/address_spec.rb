6796956a87a7dedef356085309fd8e42f8f672a1
max <laterty@gmail.com>
2022-11-28 23:46:07 +0200

    Address spec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'table' do
+    it do
+      is_expected.to have_db_column(:first_name).of_type(:string).with_options(null: false)
+      is_expected.to have_db_column(:last_name).of_type(:string).with_options(null: false)
+      is_expected.to have_db_column(:address).of_type(:string).with_options(null: false)
+      is_expected.to have_db_column(:city).of_type(:string).with_options(null: false)
+      is_expected.to have_db_column(:country).of_type(:string).with_options(null: false)
+      is_expected.to have_db_column(:zip).of_type(:string).with_options(null: false)
+      is_expected.to have_db_column(:phone).of_type(:string).with_options(null: false)
+      is_expected.to have_db_column(:type).of_type(:string).with_options(null: false)
+    end
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
 end
