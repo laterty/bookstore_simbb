d77ec002fd85e4ef7975c21cfe8157643ff7f0c0
max <laterty@gmail.com>
2022-11-29 00:34:14 +0200

    final locales

--- spec/features/change_email_spec.rb
+++ spec/features/change_email_spec.rb
@@ -4,7 +4,7 @@ describe 'Change email' do
   let(:user) { create(:user) }
   let(:valid_email) { 'yolo@yolo.com' }
   let(:invalid_email) { 'yolo.com' }
-  
+
   before { log_in(user) }
 
   describe 'Visitor change email to invalid email' do
@@ -19,7 +19,7 @@ describe 'Change email' do
     it do
       expect(page).to have_current_path update_user_email_path
       expect(page).not_to have_content user.email
-      expect(page).to have_content 'Invalid email'
+      expect(page).to have_content I18n.t('validation.email.invalid')
     end
   end
 
@@ -35,7 +35,7 @@ describe 'Change email' do
     it do
       expect(page).to have_current_path privacy_settings_path
       expect(page).not_to have_content valid_email
-      expect(page).to have_content 'Email was successfully changed!'
+      expect(page).to have_content I18n.t('users.successful.update_email')
     end
     # CHECK how to check updating attributes while testing
     # expect(user.email).to eq valid_email # => false
--- spec/features/change_password_spec.rb
+++ spec/features/change_password_spec.rb
@@ -8,75 +8,109 @@ describe 'Change password' do
   let(:new_password) { 'Yolo123*' }
   let(:invalid_confirmation_new_password) { 'yolO123*' }
   let(:valid_confirmation_new_password) { new_password }
-  
+
   before { log_in(user) }
 
-  it 'visitor puts empty values in all fields' do
-    visit privacy_settings_path
-    within '#change_password_form' do
-      click_button(I18n.t('users.partials.change_password_form.submit_button'))
+  describe 'visitor puts empty values in all fields' do
+    before do
+      visit privacy_settings_path
+      within '#change_password_form' do
+        click_button(I18n.t('users.partials.change_password_form.submit_button'))
+      end
     end
-    expect(page).to have_current_path update_user_password_path
-    expect(page).to have_content "can't be blank"
+
+    it do
+      expect(page).to have_current_path update_user_password_path
+      expect(page).to have_content "can't be blank"
+    end
   end
 
-  it 'visitor puts empty values in old_passwords field' do
-    visit privacy_settings_path
-    within '#change_password_form' do
-      fill_in I18n.t('users.partials.change_password_form.label.current_password'), with: empty_string
-      fill_in I18n.t('users.partials.change_password_form.label.password'), with: new_password
-      fill_in I18n.t('users.partials.change_password_form.label.password_confirmation'), with: valid_confirmation_new_password
-      click_button(I18n.t('users.partials.change_password_form.submit_button'))
+  describe 'visitor puts empty values in old_passwords field' do
+    before do
+      visit privacy_settings_path
+      within '#change_password_form' do
+        fill_in I18n.t('users.partials.change_password_form.label.current_password'), with: empty_string
+        fill_in I18n.t('users.partials.change_password_form.label.password'), with: new_password
+        fill_in I18n.t('users.partials.change_password_form.label.password_confirmation'),
+                with: valid_confirmation_new_password
+        click_button(I18n.t('users.partials.change_password_form.submit_button'))
+      end
     end
-    expect(page).to have_current_path update_user_password_path
-    expect(page).to have_content "can't be blank"
+
+    it do
+      expect(page).to have_current_path update_user_password_path
+      expect(page).to have_content "can't be blank"
+    end
   end
 
-  it 'visitor puts unequal passwords' do
-    visit privacy_settings_path
-    within '#change_password_form' do
-      fill_in I18n.t('users.partials.change_password_form.label.current_password'), with: current_password
-      fill_in I18n.t('users.partials.change_password_form.label.password'), with: new_password
-      fill_in I18n.t('users.partials.change_password_form.label.password_confirmation'), with: invalid_confirmation_new_password
-      click_button(I18n.t('users.partials.change_password_form.submit_button'))
+  describe 'visitor puts unequal passwords' do
+    before do
+      visit privacy_settings_path
+      within '#change_password_form' do
+        fill_in I18n.t('users.partials.change_password_form.label.current_password'), with: current_password
+        fill_in I18n.t('users.partials.change_password_form.label.password'), with: new_password
+        fill_in I18n.t('users.partials.change_password_form.label.password_confirmation'),
+                with: invalid_confirmation_new_password
+        click_button(I18n.t('users.partials.change_password_form.submit_button'))
+      end
     end
-    expect(page).to have_current_path update_user_password_path
-    expect(page).to have_content 'Password confirmation should be the same with new password'
+
+    it do
+      expect(page).to have_current_path update_user_password_path
+      expect(page).to have_content I18n.t('validation.password.confirmation')
+    end
   end
 
-  it 'visitor puts invalid new passwords' do
-    visit privacy_settings_path
-    within '#change_password_form' do
-      fill_in I18n.t('users.partials.change_password_form.label.current_password'), with: current_password
-      fill_in I18n.t('users.partials.change_password_form.label.password'), with: invalid_password
-      fill_in I18n.t('users.partials.change_password_form.label.password_confirmation'), with: invalid_password
-      click_button(I18n.t('users.partials.change_password_form.submit_button'))
+  describe 'visitor puts invalid new passwords' do
+    before do
+      visit privacy_settings_path
+      within '#change_password_form' do
+        fill_in I18n.t('users.partials.change_password_form.label.current_password'), with: current_password
+        fill_in I18n.t('users.partials.change_password_form.label.password'), with: invalid_password
+        fill_in I18n.t('users.partials.change_password_form.label.password_confirmation'), with: invalid_password
+        click_button(I18n.t('users.partials.change_password_form.submit_button'))
+      end
     end
-    expect(page).to have_current_path update_user_password_path
-    expect(page).to have_content 'Complexity requirement not met. Please use: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+
+    it do
+      expect(page).to have_current_path update_user_password_path
+      expect(page).to have_content I18n.t('validation.password.complexity')
+    end
   end
 
-  it 'visitor puts wrong current password' do
-    visit privacy_settings_path
-    within '#change_password_form' do
-      fill_in I18n.t('users.partials.change_password_form.label.current_password'), with: invalid_password
-      fill_in I18n.t('users.partials.change_password_form.label.password'), with: new_password
-      fill_in I18n.t('users.partials.change_password_form.label.password_confirmation'), with: valid_confirmation_new_password
-      click_button(I18n.t('users.partials.change_password_form.submit_button'))
+  describe 'visitor puts wrong current password' do
+    before do
+      visit privacy_settings_path
+      within '#change_password_form' do
+        fill_in I18n.t('users.partials.change_password_form.label.current_password'), with: invalid_password
+        fill_in I18n.t('users.partials.change_password_form.label.password'), with: new_password
+        fill_in I18n.t('users.partials.change_password_form.label.password_confirmation'),
+                with: valid_confirmation_new_password
+        click_button(I18n.t('users.partials.change_password_form.submit_button'))
+      end
     end
-    expect(page).to have_current_path update_user_password_path
-    expect(page).to have_content 'Wrong current password'
+
+    it do
+      expect(page).to have_current_path update_user_password_path
+      expect(page).to have_content I18n.t('validation.password.current')
+    end
   end
 
-  it 'visitor did everything right' do
-    visit privacy_settings_path
-    within '#change_password_form' do
-      fill_in I18n.t('users.partials.change_password_form.label.current_password'), with: current_password
-      fill_in I18n.t('users.partials.change_password_form.label.password'), with: new_password
-      fill_in I18n.t('users.partials.change_password_form.label.password_confirmation'), with: valid_confirmation_new_password
-      click_button(I18n.t('users.partials.change_password_form.submit_button'))
+  describe 'visitor did everything right' do
+    before do
+      visit privacy_settings_path
+      within '#change_password_form' do
+        fill_in I18n.t('users.partials.change_password_form.label.current_password'), with: current_password
+        fill_in I18n.t('users.partials.change_password_form.label.password'), with: new_password
+        fill_in I18n.t('users.partials.change_password_form.label.password_confirmation'),
+                with: valid_confirmation_new_password
+        click_button(I18n.t('users.partials.change_password_form.submit_button'))
+      end
     end
-    expect(page).to have_current_path new_user_session_path
-    expect(page).to have_content 'Password was successfully changed!'
+
+    it do
+      expect(page).to have_current_path new_user_session_path
+      expect(page).to have_content I18n.t('users.successful.update_password')
+    end
   end
 end
