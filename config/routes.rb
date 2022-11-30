537b38d5bc25a4a271f6e55464e1b68d98591908
max <laterty@gmail.com>
2022-11-30 21:56:48 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,7 @@ Rails.application.routes.draw do
     resources :books, only: %i[index]
   end
   
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    sessions: 'users/sessions'
-  }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'home#index'
 
   devise_scope :user do
