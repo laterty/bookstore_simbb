75bf01f2d4dbec18d9a6d5b316974a29043ba121
max <laterty@gmail.com>
2022-11-30 22:00:46 +0200

    move password validation to custom validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,5 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: %i[facebook]
 
   validates :email, :password, presence: true
-
-  validate :password_complexity
-
-  PASSWORD_REGEX = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
-
-  def password_complexity
-    return if password.blank? || password =~ PASSWORD_REGEX
-
-    errors.add :password, I18n.t('devise.passwords.complexity_error')
-  end
-
-  def self.from_omniauth(auth)
-    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-    end
-  end
+  validates :password, complexity_format: true
 end
