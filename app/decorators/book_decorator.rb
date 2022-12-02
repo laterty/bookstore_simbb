c99eeff4052a023fb16db38e09e0946503115ee1
max <laterty@gmail.com>
2022-12-02 18:23:09 +0200

    short_description book decoration

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -2,8 +2,13 @@
 
 class BookDecorator < Draper::Decorator
   delegate_all
+  SHORT_DESCRIPTION_LENGTH = 250
 
   def authors_names
     authors.pluck(:name).join(',')
+  end
+
+  def short_description
+    description.truncate(SHORT_DESCRIPTION_LENGTH)
   end
 end
