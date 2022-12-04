654e62792eb3ba7d8634be615a0565fdfda712ad
max <laterty@gmail.com>
2022-12-05 00:40:46 +0200

    fix specs

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Review, type: :model do
     it { is_expected.to have_db_column(:title).of_type(:string).with_options(null: false) }
     it { is_expected.to have_db_column(:content).of_type(:text).with_options(null: false) }
     it { is_expected.to have_db_column(:rating).of_type(:integer).with_options(null: false) }
-    it { is_expected.to have_db_column(:status).of_type(:integer).with_options(null: false, default: 0) }
+    it { is_expected.to have_db_column(:status).of_type(:integer).with_options(null: false) }
   end
 
   describe 'associations' do
-    it { is_expected.to belongs_to(:book) }
-    it { is_expected.to belongs_to(:user) }
+    it { is_expected.to belong_to(:book) }
+    it { is_expected.to belong_to(:user) }
   end
 end
