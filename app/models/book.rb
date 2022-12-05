7bcf1ffe4d58bb24e654b4e3341151028fc21edd
max <laterty@gmail.com>
2022-12-05 02:59:14 +0200

    add association to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,7 @@ class Book < ApplicationRecord
   belongs_to :category
   has_many :author_books, dependent: :destroy
   has_many :authors, through: :author_books
+  has_many :reviews, dependent: :destroy
 
   validates :title, :description, :price, :year_of_publication, :materials, :dimensions, presence: true
   validates :price, numericality: { greater_than: MIN_PRICE_VALUE }
