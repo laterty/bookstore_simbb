64c2fc177b7a2ea646d7c100382908dddbdb7f35
max <laterty@gmail.com>
2022-12-10 00:39:06 +0200

    add default_image

--- app/uploaders/book_image_uploader.rb
+++ app/uploaders/book_image_uploader.rb
@@ -2,37 +2,13 @@
 
 class BookImageUploader < ApplicationUploader
   ALLOWED_IMAGE_FORMATS = %w[jpg jpeg gif png].freeze
+  DEFAULT_IMAGE_URL = 'default_book_image.png'
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
-
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
+  def default_url(*)
+    DEFAULT_IMAGE_URL
+  end
 
   def extension_allowlist
     ALLOWED_IMAGE_FORMATS
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 end
