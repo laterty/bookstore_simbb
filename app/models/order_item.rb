0900e307d821a6d00ebfe856749bf3b787901661
max <laterty@gmail.com>
2022-12-12 18:47:59 +0200

    validation order items quantity

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class OrderItem < ApplicationRecord
+  MIN_QUANTITY = 1
+
   belongs_to :order
   belongs_to :book
+
+  validates :quantity, numericality: { greater_than_or_equal_to: MIN_QUANTITY }
 end
