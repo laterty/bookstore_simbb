3b1b20bc6af0ed48bbaee7746302db22382ffd86
max <laterty@gmail.com>
2022-12-12 16:37:16 +0200

    finish specs

--- spec/features/catalog_spec.rb
+++ spec/features/catalog_spec.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Catalog', type: :feature do
-  before do
-    create_list(:book, 6)
-  end
-
+#  before { create_list(:book, 6) }
   describe 'link to catalog from menu' do
     before do
+      create_list(:book, 6)
       visit root_path
       within('div.hidden-xs') do
         find('.nav.navbar-nav').click_link(I18n.t('layouts.header.navigation_shop'))
@@ -19,6 +17,7 @@ RSpec.describe 'Catalog', type: :feature do
 
   describe 'sorting by price asc' do
     before do
+      create_list(:book, 6)
       visit books_path
       within('div.hidden-xs.clearfix') do
         find('a.dropdown-toggle.lead.small').click
@@ -36,6 +35,7 @@ RSpec.describe 'Catalog', type: :feature do
 
   describe 'sorting by price desc' do
     before do
+      create_list(:book, 6)
       visit books_path
       within('div.hidden-xs.clearfix') do
         find('a.dropdown-toggle.lead.small').click
@@ -48,6 +48,34 @@ RSpec.describe 'Catalog', type: :feature do
     it 'Change the order of displayed items price: high to low' do
       expect(page).to have_current_path books_path, ignore_query: true
       expect(all('.title')[3].text).to eq books_price_high_to_low[3].title
+    end
+  end
+
+  describe 'cover_image' do
+    context 'when book has cover_image' do
+      let!(:book) { create(:book) }
+      let(:expected_result) { /#{book.cover_image}/ }
+      let(:result) { page.find('img', class: 'img-shadow general-thumbnail-img')['src'] }
+
+      before { visit books_path }
+
+      it 'user can see cover_image on page' do
+        expect(result).to match(expected_result)
+      end
+    end
+
+    context 'when book without cover_image' do
+      let(:book_image_default) { /#{BookImageUploader::DEFAULT_IMAGE_URL[0..-5]}/ }
+      let(:book_image) { page.find('img', class: 'img-shadow general-thumbnail-img')['src'] }
+
+      before do
+        create(:book, cover_image: nil)
+        visit books_path
+      end
+
+      it 'user can see default image on page' do
+        expect(book_image).to match(book_image_default)
+      end
     end
   end
 end
