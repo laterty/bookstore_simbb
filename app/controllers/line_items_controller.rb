19eff3cb97d62d4b93fe07138dd5be0ba732eaf8
max <laterty@gmail.com>
2022-12-13 15:38:42 +0200

    carts view

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,7 +5,6 @@ class LineItemsController < ApplicationController
     # Find associated product and current cart
     chosen_product = Product.find(params[:product_id])
     current_cart = @current_cart
-  
     # If cart already has this product then find the relevant line_item and iterate quantity otherwise create a new line_item for this product
     if current_cart.products.include?(chosen_product)
       # Find the line_item with the chosen_product
@@ -17,7 +16,6 @@ class LineItemsController < ApplicationController
       @line_item.cart = current_cart
       @line_item.product = chosen_product
     end
-  
     # Save and redirect to cart show path
     @line_item.save
     redirect_to cart_path(current_cart)
