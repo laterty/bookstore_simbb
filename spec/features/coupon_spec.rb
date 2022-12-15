66f6bd1e668cd28b333ab3b813acec77cc1a3914
max <laterty@gmail.com>
2022-12-15 21:14:07 +0200

    fix specs

--- spec/features/coupon_spec.rb
+++ spec/features/coupon_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe 'Cart page', type: :feature do
     before do
       page.set_rack_session(cart_id: cart.id)
       visit cart_path(cart)
-      apply_coupon
+      apply_coupon(coupon_code)
     end
 
     it 'shows total price' do
@@ -41,6 +41,52 @@ RSpec.describe 'Cart page', type: :feature do
 
     it 'shows discount' do
       expect(discount).to eq(cart.discount)
+    end
+
+    it 'show message' do
+      expect(page).to have_text(I18n.t('carts.coupon.valid_coupon'))
+    end
+  end
+
+  context 'when user fill in with invalid coupon' do
+    let(:invalid_coupon_code) { FFaker::BaconIpsum.word }
+    let(:discount) do
+      within('table.general-summary-table') do
+        find_by_id('cart-discount').text.to_d
+      end
+    end
+
+    before do
+      page.set_rack_session(cart_id: cart.id)
+      visit cart_path(cart)
+      apply_coupon(invalid_coupon_code)
+    end
+
+    it 'show message' do
+      expect(page).to have_text(I18n.t('carts.coupon.undefined_coupon'))
+    end
+  end
+
+  context 'when coupon has been already used' do
+    let(:another_cart) { create(:cart) }
+    let(:coupon) { create(:coupon) }
+    let(:coupon_code) { coupon.code }
+    let(:discount) do
+      within('table.general-summary-table') do
+        find_by_id('cart-discount').text.to_d
+      end
+    end
+
+    before do
+      coupon.cart = another_cart
+      coupon.save
+      page.set_rack_session(cart_id: cart.id)
+      visit cart_path(cart)
+      apply_coupon(coupon_code)
+    end
+
+    it 'show message' do
+      expect(page).to have_text(I18n.t('validation.coupon.uniqueness'))
     end
   end
 end
--- spec/support/helpers/apply_coupon.rb
+++ spec/support/helpers/apply_coupon.rb
@@ -2,7 +2,7 @@
 
 module Helpers
   module ApplyCoupon
-    def apply_coupon
+    def apply_coupon(coupon_code)
       within('form.simple_form.coupon') do
         fill_in I18n.t('carts.partials.coupon_form.label'), with: coupon_code
         click_button(I18n.t('carts.partials.coupon_form.submit_button'))
