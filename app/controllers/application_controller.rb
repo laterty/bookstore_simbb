5db4d2bcfd3d04e6e8c95ec5e600932454569917
max <laterty@gmail.com>
2022-12-16 00:21:17 +0200

    refactor app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,21 +5,34 @@ class ApplicationController < ActionController::Base
   before_action :categories_all, :current_cart
 
   def categories_all
-    @categories_all ||= Category.joins(:books).select('categories.*, COUNT(books.id) as books_count').group(:id)
+    @categories_all ||= Category.joins(:books)
+                                .select('categories.*, COUNT(books.id) as books_count')
+                                .group(:id)
   end
 
-  def current_cart
-    if session[:cart_id]
-      cart = Cart.joins(:line_items).where(id: session[:cart_id]).select('carts.*, COUNT(line_items.id) as line_items_count').group(:id)
-      if cart.present?
-        @current_cart = cart.first.decorate
-      else
-        session[:cart_id] = nil
-      end
-    end
-    return unless session[:cart_id].nil?
+  private
 
+  def current_cart
+    restore_cart_from_session if session[:cart_id]
+
+    new_cart unless session[:cart_id]
+  end
+
+  def restore_cart_from_session
+    return session[:cart_id] = nil if cart.blank?
+
+    @current_cart = cart.first.decorate
+  end
+
+  def new_cart
     @current_cart = Cart.create.decorate
     session[:cart_id] = @current_cart.id
+  end
+
+  def cart
+    Cart.joins(:line_items)
+        .where(id: session[:cart_id])
+        .select('carts.*, COUNT(line_items.id) as line_items_count')
+        .group(:id)
   end
 end
