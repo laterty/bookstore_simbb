edf3011b24c606b02b90fe70d4f573878acccba0
max <laterty@gmail.com>
2022-12-16 23:54:51 +0200

    add uniq index to line items

--- db/migrate/20221212185407_create_line_items.rb
+++ db/migrate/20221212185407_create_line_items.rb
@@ -1,11 +1,13 @@
 class CreateLineItems < ActiveRecord::Migration[6.1]
   def change
     create_table :line_items do |t|
-      t.references :book, null: false, foreign_key: true
-      t.references :cart, null: false, foreign_key: true
+      t.references :book, null: false, foreign_key: true, index: false
+      t.references :cart, null: false, foreign_key: true, index: false
       t.integer :quantity, null: false, default: 1
 
       t.timestamps
     end
+
+    add_index :line_items, %i[cart_id book_id], unique: true
   end
 end
