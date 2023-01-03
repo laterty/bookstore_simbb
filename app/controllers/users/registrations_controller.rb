e7b566ca5417bc7a1b1c80255ce6ed3b28b0b2cc
max <laterty@gmail.com>
2023-01-03 16:11:59 +0200

    redefine registration controller

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    def create
+      quick_registration? ? quick_registration : super
+    end
+
+    private
+
+    def quick_registration
+      params[:user][:password] = params[:user][:password_confirmation] = devise_password
+      build_resource(sign_up_params)
+      resource.save ? authenticate_user : redirect_back_and_show_errors
+    end
+
+    def authenticate_user
+      sign_up(resource_name, resource)
+      resource.send_reset_password_instructions
+      redirect_to(checkout_path, notice: t('devise.quick_registration.message.password_instructions'))
+    end
+
+    def devise_password
+      Devise.friendly_token[User::START_PASSWORD_LENGTH, User::END_PASSWORD_LENGTH]
+    end
+
+    def quick_registration?
+      params[:user][:quick].present?
+    end
+
+    def redirect_back_and_show_errors
+      redirect_to cart_path, alert: resource.errors.full_messages.to_sentence
+    end
+  end
+end
