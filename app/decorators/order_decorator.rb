b7a92fefc9372d2a43b01ab2c7841997c1b43a39
max <laterty@gmail.com>
2023-01-09 21:13:23 +0200

    order decorator

---
+++ app/decorators/order_decorator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class OrderDecorator < Draper::Decorator
+  delegate_all
+  decorates_association :order_items
+
+  def subtotal_price
+    order_items.sum { |item| item.quantity * item.book_price }
+  end
+
+  def discount_price
+    discount ? (subtotal_price * discount / Coupon::MAX_DISCOUNT).round(2) : Coupon::MIN_DISCOUNT
+  end
+
+  def total_price
+    subtotal_price - discount
+  end
+end
